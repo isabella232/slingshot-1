34192469e0349fd61397871da2821a6444ba8226
Dinesh Shenoy <dinesh.shenoy@protocol.ai>
2020-12-08 15:00:11 -0800

    Code updates

--- example/extract.py
+++ example/extract.py
@@ -4,7 +4,7 @@ import json
 
 S3_URL = "https://nyc-tlc.s3.amazonaws.com"
 LOTUS_NODE = "http://34.216.9.61:28001/rpc/v0"
-TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBbGxvdyI6WyJyZWFkIiwid3JpdGUiLCJzaWduIiwiYWRtaW4iXX0.B4qGPtfsF7KwK1kg0nakQbaJC3-SyxqYPKvW1hU7GKU"
+TOKEN = "REPLACE_ME"
 # S3_ALT_URL = "https://s3.amazonaws.com/nyc-tlc"
 
 datatypes = ["yellow", "green", "fhv", "hvfhv"]
@@ -18,18 +18,31 @@ def lotus_cmd(method,data={}, files=[], debug=False):
     headers = {"Authorization": "Bearer " + TOKEN}
     data["jsonrpc"] = "2.0"
     data["method"] = "Filecoin." + method
+
+    # This may need to be custom - make it part of the parameters passed in?
     data["id"] = 2
     data["params"] = []
+
     r = requests.post(LOTUS_NODE, data=json.dumps(data), headers=headers)
     # print(r.status_code)
     # print(r.reason)
     j = json.loads(r.text)
     return j['result']
 
+
+# Can't import files into the Lotus library remotely
+def client_import(file):
+    files={'files': open(filename,'rb')}
+    values={'upload_file' : 'file.txt' , 'DB':'photcat' , 'OUT':'csv' , 'SHORT':'short'}
+    r=requests.post(url,files=files,data=values)
+    print ("Importing file to Lotus")
+    return cid
+
+
 def download_file(url):
     filename = url.split("/")[-1] 
     print ("Downloading: " + filename)
-    with open(filename, 'wb') as f:
+    with open('output/' + filename, 'wb') as f:
         response = requests.get(url, stream=True)
         total_length = response.headers.get('content-length')
 
@@ -48,26 +61,32 @@ def download_file(url):
                 sys.stdout.flush()
     f.close()
 
-# Can't import files into the Lotus library remotely
-def client_import(file):
-    files={'files': open(filename,'rb')}
-    values={'upload_file' : 'file.txt' , 'DB':'photcat' , 'OUT':'csv' , 'SHORT':'short'}
-    r=requests.post(url,files=files,data=values)
 
-    print ("Importing file to Lotus")
+def assemble():
+    output_list = {}
+
+    for file in os.dirlist():
+        if output_list[year] is None:
+            output_list[year] = [filename]
+        else:
+            output_list[year].append(filename)
+
+    json.dumps(output_list)
+
 
 
 def main(argv):
 
     response = lotus_cmd("ChainHead")
-    print(response)
-    sys.exit(0)
+    # print(response)
+
+    # for testing
     # year = "2019"
     # datatype = "yellow"
     # month = "06"
 
 
-    for year in range(2009, 2021): 
+    for year in range(2009, 2021):
         for month in range(1, 13):
             for datatype in datatypes:
                 filename = datatype + "_tripdata_" + str(year) + "-" + str(month).zfill(2) + ".csv"
@@ -75,9 +94,11 @@ def main(argv):
                 url = S3_URL + "/trip+data/" + filename
                 # print (url)
                 download_file(url)
-                cid = client_import(filename)
+
                 # make deal
-                break    # Temporary to test for one month
+        break # pause after one year
+
+    assemble()
 
 
 if __name__ == "__main__":
