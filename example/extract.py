3c1a90183d7e73352a1e9ab1b6528807f8e3e527
Dinesh Shenoy <dinesh.shenoy@protocol.ai>
2020-12-20 22:20:01 -0800

    Simplifying extract.py

--- example/extract.py
+++ example/extract.py
@@ -1,42 +1,16 @@
 import sys
 import requests
 import json
+import os
 
 S3_URL = "https://nyc-tlc.s3.amazonaws.com"
-LOTUS_NODE = "http://34.216.9.61:28001/rpc/v0"
-TOKEN = "REPLACE_ME"
-# S3_ALT_URL = "https://s3.amazonaws.com/nyc-tlc"
+# S3_ALT_URL = "https://s3.amazonaws.com/nyc-tlc"  (for fhv, hvfhv files)
 
 datatypes = ["yellow", "green", "fhv", "hvfhv"]
 
-# Example:  https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2019-06.csv
-#             https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2019-06.csv
-
-
-def lotus_cmd(method,data={}, files=[], debug=False):
-    # Do a chain API call
-    headers = {"Authorization": "Bearer " + TOKEN}
-    data["jsonrpc"] = "2.0"
-    data["method"] = "Filecoin." + method
-
-    # This may need to be custom - make it part of the parameters passed in?
-    data["id"] = 2
-    data["params"] = []
-
-    r = requests.post(LOTUS_NODE, data=json.dumps(data), headers=headers)
-    # print(r.status_code)
-    # print(r.reason)
-    j = json.loads(r.text)
-    return j['result']
-
-
-# Can't import files into the Lotus library remotely
-def client_import(file):
-    files={'files': open(filename,'rb')}
-    values={'upload_file' : 'file.txt' , 'DB':'photcat' , 'OUT':'csv' , 'SHORT':'short'}
-    r=requests.post(url,files=files,data=values)
-    print ("Importing file to Lotus")
-    return cid
+# Example URLs:
+# https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2019-06.csv
+# https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2019-06.csv
 
 
 def download_file(url):
@@ -45,6 +19,7 @@ def download_file(url):
     with open('output/' + filename, 'wb') as f:
         response = requests.get(url, stream=True)
         total_length = response.headers.get('content-length')
+        # TODO: continue if you can't find the resource
 
         if total_length is None: # no content length header
             f.write(response.content)
@@ -65,26 +40,21 @@ def download_file(url):
 def assemble():
     output_list = {}
 
-    for file in os.dirlist():
-        if output_list[year] is None:
-            output_list[year] = [filename]
-        else:
-            output_list[year].append(filename)
+    for file in os.listdir('output'):
+        n = file.find("_tripdata_") + len("_tripdata") + 1
+        year = file[n:n+4]
 
-    json.dumps(output_list)
+        if year not in output_list.keys():
+            output_list[year] = [file]
+        else:
+            output_list[year].append(file)
+    return output_list
 
 
 
 def main(argv):
-
-    response = lotus_cmd("ChainHead")
-    # print(response)
-
-    # for testing
-    # year = "2019"
-    # datatype = "yellow"
-    # month = "06"
-
+    assemble()
+    sys.exit(1)
 
     for year in range(2009, 2021):
         for month in range(1, 13):
@@ -93,12 +63,13 @@ def main(argv):
 
                 url = S3_URL + "/trip+data/" + filename
                 # print (url)
-                download_file(url)
+                if not os.path.exists('output/' + filename):
+                    download_file(url)
+                else:
+                    print(filename + " already exists - skipping...")
+        # break # pause after one year
 
-                # make deal
-        break # pause after one year
 
-    assemble()
 
 
 if __name__ == "__main__":
